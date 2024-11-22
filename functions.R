66badffd70784eb96c9852286e44b99f2f16651b
Jesse Lecy <jdlecy@gmail.com>
2024-11-22 08:46:37 -0500

    Update functions.R

--- functions.R
+++ functions.R
@@ -34,6 +34,7 @@ get_bmf <- function( inactive=TRUE ){
   return( bmf )
 }
 
+
 format_ein <- function( x, to="id" ) {
 
     if( to == "id" ){   
@@ -45,6 +46,22 @@ format_ein <- function( x, to="id" ) {
       
     if( to == "n" ){  
       x <- gsub( "[^0-9]", "", x )
-      return( x ) }
-      
+      return( x ) }    
+}
+
+
+# df.list <- list()
+# df.list[[ "2009" ]] <- get_table( "F9-P01-T00-SUMMARY", year=2009 ) 
+# df.list[[ "2010" ]] <- get_table( "F9-P01-T00-SUMMARY", year=2010 ) 
+# df.list[[ "2011" ]] <- get_table( "F9-P01-T00-SUMMARY", year=2011 ) 
+# df <- dplyr::bind_rows( df.list )
+
+get_panel <- function( table.name, years=2009:2019 ){
+  df.list <- list()
+  for( i in years )
+  {
+    df.list[[ "2009" ]] <- get_table( "F9-P01-T00-SUMMARY", year=2009 ) 
+  }
+  df <- dplyr::bind_rows( df.list )
+  return( df )
 }
